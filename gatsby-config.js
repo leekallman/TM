41b1d6762367c818a0318447a2ba3ae746fc1706
Lisa Lee <lisaleekallman@gmail.com>
2021-02-09 19:54:14 +0100

    removed unused files

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,21 +15,6 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    // {
-    // resolve: 'gatsby-transformer-remark',
-    //   options: {
-    //     plugins: [
-    //       'gatsby-remark-relative-images',
-    //       {
-    //         resolve: 'gatsby-remark-images',
-    //         options: {
-    //           maxWidth: 750,
-    //           linkImagesToOriginal: false
-    //         }
-    //       }
-    //     ]
-    //   }
-    // }
   ],
 }
  
