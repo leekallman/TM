f0f2dee3d591e7e65f3594bb79477145434c779a
Lisa Lee <lisaleekallman@gmail.com>
2021-02-09 22:24:02 +0100

    asses og image

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,8 @@ import useSiteMetadata from '../components/use-siteMetadata'
             <meta property="og:title" content={title} />
             <meta property="og:type" content="website" />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content="" />
-            <meta property="og:locale" content="" />
+            <meta property="og:image" content="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/148856643_437326214370011_2170845881351565213_n.jpg?_nc_cat=102&ccb=3&_nc_sid=09cbfe&_nc_ohc=yglJzYN9czwAX-U9Zb6&_nc_ht=scontent-arn2-1.xx&oh=c7fa16107486f97810a9c010d0325f2e&oe=60477BDD" />
+            <meta property="og:locale" content="en_US" />
             <meta property="og:url" content={url} />
             <link rel="canonical" href={url}/>
           </Helmet>
