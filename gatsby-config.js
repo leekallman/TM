c519b745e7100bdb8d3835b8e360dcf7aecb0f08
Lisa Lee <lisaleekallman@gmail.com>
2021-02-10 14:22:48 +0100

    meta data fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: `Tort Magazine`,
     description: `Independent printed magazine that explores present societal issues through honest and in-depth conversations with individuals directly or indirectly questioning norms and are committed to make a change. Editorial team: Liz Olofsson, Lisa Lee, Marta Casagrande, Tor Westerlund.`,
     author: `Lisa Lee`,
-    keywords: `magazine, swedish, stockholm, sex, sex work, art, race, opression`,
     url: `https://www.tortmagazine.com`
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import './main.css';
+import favicon from '../../static.favicon.ico'
 import Hero from '../components/hero.js';
 import Posts from '../components/posts.js';
 import Footer from '../components/footer';
@@ -8,7 +9,7 @@ import useSiteMetadata from '../components/use-siteMetadata'
 
     const IndexPage = (props) => {
 
-      const {title, description, keywords, author, url} = useSiteMetadata();
+      const {title, description, author, url} = useSiteMetadata();
 
       useEffect(() => {
         window.scrollTo(0, 0)
@@ -20,7 +21,6 @@ import useSiteMetadata from '../components/use-siteMetadata'
           <Helmet>
             <title>{title}</title>
             <meta name="description" content={description}/>
-            <meta name= "keywords" content={keywords} />
             <meta name= "author" content={author} />
             <meta property="og:title" content={title} />
             <meta property="og:type" content="website" />
@@ -29,7 +29,7 @@ import useSiteMetadata from '../components/use-siteMetadata'
             <meta property="og:locale" content="en_US" />
             <meta property="og:url" content={url} />
             <link rel="canonical" href={url}/>
-            <link rel="shortcut icon" href="../../static/favicon.ico"></link>
+            <link rel="shortcut icon" href={favicon}></link>
           </Helmet>
           <section className="home"> 
             <Hero />
