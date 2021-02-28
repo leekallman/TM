47e151f0c5a97b355a6861977cfe3391d3b3df08
Lisa Lee <lisaleekallman@gmail.com>
2021-02-28 19:18:16 +0100

    re-arranged the about text

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,22 +14,23 @@ const Footer = () => {
             <div className={footerStyles.about}>
                 <p>ISSUE ONE<br/>
                 Unwritten rules exist everywhere, in every culture and religion. In the world of art and on the dance scene, in sexual relations as well as in marriages. Even among species, we have notions of the correct behavior. Rarely we do remember the creator of these norms – more often we know who broke them. Tort explores present societal conceptions through honest conversations with individuals committed to make a change.      
+                </p><br/><br/>
+                <p>
+                Editorial team: Liz Olofsson, Lisa Lee, Marta Casagrande, Tor Westerlund<br/><br/>
+                Contributors: Ashik Zaman, Riin Rannu, Maja Winberg, Thulani Gushman, Clarice Goulart, William Harvey Howe, Emilia Stålhammar
                 </p>
             </div>
 
             <div className={footerStyles.column1}>
                 <p>
-                    Editorial team: Liz Olofsson, Lisa Lee, Marta Casagrande, Tor Westerlund<br/><br/>
-                    Contributors: Ashik Zaman, Riin Rannu, Maja Winberg, Thulani Gushman, Clarice Goulart, William Harvey Howe, Emilia Stålhammar
-                    <br/><br/>
                     112 pp, 16 × 22 cm, softcover: 300g Lynx Rough, 2021.<br/>
                     Binding: Sewed glue binding<br/>
                     ISBN 2003-8003<br/>
                     100SEK
+                    <br/><br/>
+                    Stockists: <a href="https://papercutshop.se/">Papercut</a>, Krukmakargatan 24 Stockholm
                     <br/>
-                    Stockists: <a href="https://papercutshop.se/">Papercut</a>, Krukmakargatan 24 Stockholm, 
-                    Konst-ig, 
-                    Skånegatan 71, Stockholm
+                    Konst-ig, Skånegatan 71, Stockholm
                 </p>
             </div>
 
