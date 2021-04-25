d0faf02d682788a3ef3b237e81a87902798879f9
Lisa Lee <lisaleekallman@gmail.com>
2021-04-25 13:34:14 +0200

    config issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
-        accessToken: process.GATSBY_env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       }
     },
   ],
