4236e4bc637225a13fcfc272e856cb822065e24e
Lisa Lee <lisaleekallman@gmail.com>
2021-04-26 13:11:59 +0200

    testing again

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: lknoe36ir5hh,
-        accessToken: KD4YI - jd4dnEn4zuzZ4MhLofTQv_COCvGWkMMeHdHBE
+        spaceId: "lknoe36ir5hh",
+        accessToken: "KD4YI-jd4dnEn4zuzZ4MhLofTQv_COCvGWkMMeHdHBE"
       }
     },
   ],
